489d7416334f2e12b8fda012e52253b711132139
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-01-30 12:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// Complete the js code
-function Car(make, model) {}
+// Constructor function for Car
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method added to Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
+
+// Constructor function for SportsCar inheriting from Car
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor with the make and model arguments
+  Car.call(this, make, model);
+  
+  // Add a property specific to SportsCar
+  this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Set the constructor property of SportsCar to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Method added to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
